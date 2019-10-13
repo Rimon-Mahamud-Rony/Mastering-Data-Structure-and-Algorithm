d7f1824f1d4feabba2a2ac8cc76e68fbc1f35415
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2019-10-14 00:19:22 +0600

    clean

--- start cout/abdu_bari.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-///using git , it will be uploaded on github.
-int main()
-{
-    cout<<"I will complete the abdul bari course in one month"<<endl;
-    return 0;
-}
